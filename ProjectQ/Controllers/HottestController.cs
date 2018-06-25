3359612b40849ba6d957eb2a104afb9fe92a5c7d
Matt Rickard <matt.rickard@certasenergy.co.uk>
2018-06-25 22:51:26 +0100

    WIP Created model for forecast XML Sketched out code for controller

---
+++ ProjectQ/Controllers/HottestController.cs
@@ -0,0 +1,50 @@
+﻿using ProjectQ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ProjectQ.Controllers
+{
+    public class HottestController : Controller
+    {
+        // GET: Hottest
+        public ActionResult Index()
+        {
+            // get current location
+            // google maps api
+            // if fail return View("Centre of the earth")
+
+            // create open weathermap query string
+
+            // request city list in load into xml doc
+
+            var serializer = new XmlSerializer(typeof(CityList));
+            using (var reader = XmlReader.Create("~\App_Data\forecast.xml"))
+            {
+                CityList cityList = (CityList)serializer.Deserialize(reader);
+                List<CityListItem> Cities = cityList.CityListItems;
+                
+            }
+
+            // find the higest temperature using xpath max query
+
+            // iterate through the the city list and match each city with hightest temp value
+
+            // calculate distance between current location and hot city
+
+            // if city is closest set it as a match
+
+            // if closest is here  - View (Hurrah - stay right where you are)
+
+            // if closest city is elsewhere
+            //  update "get moving model"
+            //  View (GetMoving)
+
+            return View();
+        }
+    }
+}
---
+++ ProjectQ/Models/CityList.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace ProjectQ.Models
+{
+    public class CityCoord
+    {
+        public string lon { get; set; }
+        public string lat { get; set; }
+    }
+
+    public class CitySunRise
+    {
+        public string lat { get; set; }
+        public string set { get; set; }
+    }
+
+    public class City
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public CityCoord coord { get; set; }
+        public string country { get; set; }
+        [XmlAttribute("sun rise")]
+        public CitySunRise sunRise { get; set; }
+    }
+
+    public class Temperature
+    {
+        public decimal value{ get; set; }  // we need this value so make sure it's the right type
+        public string min { get; set; } 
+        public string max { get; set; }
+        public string unit { get; set; }
+    }
+
+    public class Humidity
+    {
+        public string value { get; set; }
+        public string unit { get; set; }
+    }
+
+    public class Pressure
+    {
+        public string value { get; set; }
+        public string unit { get; set; }
+    }
+
+    public class WindSpeed
+    {
+        public string value { get; set; }
+        public string name { get; set; }
+    }
+
+    public class WindDirection
+    {
+        public string value { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class WindGust
+    {
+        public string value { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Wind
+    {
+        public WindSpeed speed { get; set; }
+        public WindDirection direction { get; set; }
+        public WindGust gust { get; set; }
+    }
+
+    public class Clouds
+    {
+        public string value { get; set; }
+        public string name { get; set; }
+        public string low { get; set; }
+        public string middle { get; set; }
+        public string high { get; set; }
+    }
+
+    public class Precipitation
+    {
+        public string mode { get; set; }
+    }
+
+    public class Weather
+    {
+        public string number { get; set; }
+        public string value { get; set; }
+        public string icon { get; set; }
+    }
+
+    public class LastUpdate
+    {
+        public string value { get; set; }
+        public string unix { get; set; }
+    }
+
+    public class CityListItem
+    {
+        public City city { get; set; }
+        public Temperature temperature { get; set; }
+        public Humidity humidity { get; set; }
+        public Pressure pressure { get; set; }
+        public Wind wind { get; set; }
+        public Clouds clouds { get; set; }
+        public Precipitation precipitation { get; set; }
+        public Weather weather { get; set; }
+        public LastUpdate lastupdate { get; set; }
+    }
+    
+    [XmlRoot("cities")]
+    public class CityList
+    {
+        [XmlArray("list")]
+        [XmlArrayItem("item")]
+        public List<CityListItem> CityListItems;
+    }
+
+}
